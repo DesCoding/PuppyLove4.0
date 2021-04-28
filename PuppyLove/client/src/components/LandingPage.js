4969eb5ad675dad3e309c7e8787f80e5b513d917
Des <d.stout31@yahoo.com>
2021-04-27 19:52:24 -0500

    Final heroku push

--- PuppyLove/client/src/components/LandingPage.js
+++ PuppyLove/client/src/components/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useStoreContext } from "../utils/GlobalState";
+import { Card } from "react-bootstrap";
 
 const LandingPage = () => {
   const [state, dispatch] = useStoreContext();
@@ -14,39 +15,47 @@ const LandingPage = () => {
   return (
     <div className="container-lg mt-4 welcomcontainer">
       <div className="row">
-        Welcome PuppyLovers! Use this app to search for puppies you like and
-        visit these shelters to adopt.....
+        <Card>
+          <Card.Body>
+            {" "}
+            Welcome PuppyLovers! Use this app to search for puppies you like.
+            Please conider visiting these shelters to adopt.<br></br>
+            <br></br>
+            <p>Click here to find adoptable pups in the St. Louis Area.</p>
+            <Link to="https://www.needypaws.org/">Needy Paws</Link>
+            <br></br>
+            <Link to="https://www.strayrescue.org/">Stray Rescue</Link>
+            <br></br>
+            <Link to="https://www.hsmo.org/">Humane Society</Link>
+            <br></br>
+            <Link to="https://odas.org/">Open Door Animal Sanctuary</Link>
+            <br></br>
+            <br></br>
+            <p>Click here to find adoptable pups nationwide.</p>
+            <Link to="https://www.akc.org/akc-rescue-network/">
+              AKC Rescue Network
+            </Link>
+          </Card.Body>
+        </Card>
       </div>
-      {/* commenting out redirect so that landing page can be used to welcome new and existing users */}
 
-      {/* once toke issue is fixed, when a logged in user visits the landing page, they will see the welcome message and a link to visit thier member page (no sign up or log in) */}
       <div className="row">
         {!state.isLoggedin ? (
           <>
             <div className="col-md-6 col-md-offset-3">
-              <p>If you're new here, please sign up.</p>
-              <Link to="/SignUp">SignUp</Link>
+              <Link to="/SignUp">If you're new here, please sign up here.</Link>
             </div>
             <div className="col-md-6 col-md-offset-3">
-              <p>If you're a member, please log in.</p>
-              <Link to="/LogIn">LogIn</Link>
+              <Link to="/LogIn">If you're a member, please log in here.</Link>
             </div>
           </>
         ) : (
           <div className="col-md-6 col-md-offset-3">
-            <p>Click here to search for more puppy pics.</p>
-            <Link to="/Members">Members</Link>
+            <Link to="/Members">
+              Already a logged in member, click here to visit your page
+            </Link>
           </div>
         )}
-      </div>
-      <div className="col-md-6 col-md-offset-3">
-        <p>Click here to find adoptable pups in the St. Louis Area.</p>
-        <Link to="https://www.needypaws.org/"></Link>
-        <Link to="https://www.strayrescue.org/"></Link>
-        <Link to="https://www.hsmo.org/"></Link>
-        <Link to="https://odas.org/"></Link>
-        <p>Click here to find adoptable pups nationwide.</p>
-        <Link to="https://www.akc.org/akc-rescue-network/"></Link>
       </div>
     </div>
   );
